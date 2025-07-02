08c1a66c52ff0fc8b275689cf83d7a41990a5bbb
Wagner Junior <waguinhotcho@Gmail.com>
2025-07-02 20:33:38 -0300

    teste mobile

--- src/main/java/br/com/oinkvest/config/SecurityConfig.java
+++ src/main/java/br/com/oinkvest/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package br.com.oinkvest.config;
 import br.com.oinkvest.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,12 +29,21 @@ public class SecurityConfig {
         return auth;
     }
 
+    // AuthenticationManager para uso em autenticação via API
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
     // Regras de segurança (páginas públicas e protegidas)
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            //.csrf(csrf->csrf.disable())
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/login", "/register", "/css/**", "/js/**").permitAll()
+                .requestMatchers("/login", "/register", "/css/**", "/js/**", "/api/mobile/login").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
