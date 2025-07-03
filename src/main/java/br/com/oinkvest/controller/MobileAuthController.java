41480e1f47568ab581a38383ee916bd4cd456881
Renato Alves Junior <grangamin@gmail.com>
2025-07-03 01:59:26 +0000

    feat: jsessionid no corpo mobile

--- src/main/java/br/com/oinkvest/controller/MobileAuthController.java
+++ src/main/java/br/com/oinkvest/controller/MobileAuthController.java
@@ -3,6 +3,9 @@ package br.com.oinkvest.controller;
 import br.com.oinkvest.dto.LoginRequest;
 import br.com.oinkvest.model.Usuario;
 import br.com.oinkvest.repository.UsuarioRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,12 +33,15 @@ public class MobileAuthController {
     private UsuarioRepository usuarioRepository;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-        UsernamePasswordAuthenticationToken authToken =
-                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getSenha());
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),
+                loginRequest.getSenha());
         try {
             Authentication authentication = authenticationManager.authenticate(authToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            HttpSession session = request.getSession(true);
+            String sessionId = session.getId();
 
             Usuario usuario = usuarioRepository.findByEmail(loginRequest.getEmail()).orElse(null);
             Map<String, Object> response = new HashMap<>();
@@ -43,6 +49,7 @@ public class MobileAuthController {
                 response.put("id", usuario.getId());
                 response.put("nome", usuario.getNome());
                 response.put("email", usuario.getEmail());
+                response.put("token", sessionId);
             }
             return ResponseEntity.ok(response);
         } catch (AuthenticationException ex) {
